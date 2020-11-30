4ec77ce1a20383ae59720286974459f0cef08f9f
Johanna Gerdts <jg@mavenlist.eu>
2020-11-30 11:01:59 +0100

    Temporary commit

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
-from project_fraud.lib import drop_many_missing_values
+import numpy as np
+#from project_fraud.lib import drop_many_missing_values
 import matplotlib.pyplot as plt
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -13,14 +14,14 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 
-data = drop_many_missing_values()
+#data = drop_many_missing_values()
 
 
-vals = plt.hist(data['TransactionDT'] / (3600*24), bins=1800)
-plt.xlim(70, 78)
-plt.xlabel('Days')
-plt.ylabel('Number of transactions')
-plt.ylim(0,1000)
+#vals = plt.hist(data['TransactionDT'] / (3600*24), bins=1800)
+#plt.xlim(70, 78)
+#plt.xlabel('Days')
+#plt.ylabel('Number of transactions')
+#plt.ylim(0,1000)
 
 app.layout = html.Div([
     html.H2('Hello World'),
