92cfe2565d98e0f792ec8d1ed46ad1ad439a4f23
Johanna Gerdts <jg@mavenlist.eu>
2020-11-30 10:17:09 +0100

    load data

--- app.py
+++ app.py
@@ -3,13 +3,24 @@ import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-
+import pandas as pd
+from project_fraud.lib import drop_many_missing_values
+import matplotlib.pyplot as plt
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
+
+data = drop_many_missing_values()
+
+
+vals = plt.hist(data['TransactionDT'] / (3600*24), bins=1800)
+plt.xlim(70, 78)
+plt.xlabel('Days')
+plt.ylabel('Number of transactions')
+plt.ylim(0,1000)
 
 app.layout = html.Div([
     html.H2('Hello World'),
