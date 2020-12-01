4d7ccb7810b7f94f7310ea128ed754f58e55f746
Johanna Gerdts <jg@mavenlist.eu>
2020-12-01 10:52:43 +0100

    add all

--- app.py
+++ app.py
@@ -3,42 +3,133 @@ import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
 import pandas as pd
 import numpy as np
-from project_fraud.lib import drop_many_missing_values
+#from project_fraud.project_fraud.lib import drop_many_missing_values
 import matplotlib.pyplot as plt
+import pickle
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+def loadmodel(filename):
+    loaded_model = pickle.load(open(filename, 'rb'))
+    return loaded_model
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+#Style of the app
+app = dash.Dash(external_stylesheets=[dbc.themes.SLATE])
+
 
 server = app.server
 
 
 print("Loading in data from data/")
-data = drop_many_missing_values()
+#df = drop_many_missing_values()
 
 
-#vals = plt.hist(data['TransactionDT'] / (3600*24), bins=1800)
-#plt.xlim(70, 78)
-#plt.xlabel('Days')
-#plt.ylabel('Number of transactions')
-#plt.ylim(0,1000)
-# labels  = data.country.unique()
-# values = data.country.unique()
-# dropdown_dict = [{l: v} for l, v in zip(labels, values)]
+df = pd.read_csv("~/data/data_J.csv")
 
+def generate_table(dataframe, max_rows=10):
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns])
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ])
+
+app.layout = html.Div(children=[
+    html.H4(children='Card Transactions'),
+    generate_table(df)
+])
 
 app.layout = html.Div([
-    html.H2('Hello World'),
-    dcc.Dropdown(
-        id='dropdown',
-        # options=dropdown_dict,
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
-    ),
-    html.Div(id='display-value')
+    html.Div('Dash To-Do list'),
+    dcc.Input(id="new-item"),
+    html.Button("Add", id="add"),
+    html.Button("Clear Done", id="clear-done"),
+    html.Div(id="list-container"),
+    html.Div(id="totals")
 ])
+
+style_todo = {"display": "inline", "margin": "10px"}
+style_done = {"textDecoration": "line-through", "color": "#888"}
+style_done.update(style_todo)
+
+
+@app.callback(
+    [
+        Output("list-container", "children"),
+        Output("new-item", "value")
+    ],
+    [
+        Input("add", "n_clicks"),
+        Input("new-item", "n_submit"),
+        Input("clear-done", "n_clicks")
+    ],
+    [
+        State("new-item", "value"),
+        State({"index": ALL}, "children"),
+        State({"index": ALL, "type": "done"}, "value")
+    ]
+)
+def edit_list(add, add2, clear, new_item, items, items_done):
+    triggered = [t["prop_id"] for t in dash.callback_context.triggered]
+    adding = len([1 for i in triggered if i in ("add.n_clicks", "new-item.n_submit")])
+    clearing = len([1 for i in triggered if i == "clear-done.n_clicks"])
+    new_spec = [
+        (text, done) for text, done in zip(items, items_done)
+        if not (clearing and done)
+    ]
+    if adding:
+        new_spec.append((new_item, []))
+    new_list = [
+        html.Div([
+            dcc.Checklist(
+                id={"index": i, "type": "done"},
+                options=[{"label": "", "value": "done"}],
+                value=done,
+                style={"display": "inline"},
+                labelStyle={"display": "inline"}
+            ),
+            html.Div(text, id={"index": i}, style=style_done if done else style_todo)
+        ], style={"clear": "both"})
+        for i, (text, done) in enumerate(new_spec)
+    ]
+    return [new_list, "" if adding else new_item]
+
+
+@app.callback(
+    Output({"index": MATCH}, "style"),
+    Input({"index": MATCH, "type": "done"}, "value")
+)
+def mark_done(done):
+    return style_done if done else style_todo
+
+
+@app.callback(
+    Output("totals", "children"),
+    Input({"index": ALL, "type": "done"}, "value")
+)
+def show_totals(done):
+    count_all = len(done)
+    count_done = len([d for d in done if d])
+    result = "{} of {} items completed".format(count_done, count_all)
+    if count_all:
+        result += "- {}%".format(int(100 * count_done / count_all))
+    return result
+# app.layout = html.Div([
+#     html.H2('Hello World'),
+#     dcc.Dropdown(
+#         id='dropdown',
+#         # options=dropdown_dict,
+#         options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
+#         value='LA'
+#     ),
+#     html.Div(id='display-value')
+# ])
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
