192335c134f257acad8560782e4fc4e1d17297f2
Johanna Gerdts <jg@mavenlist.eu>
2020-12-03 16:24:55 +0100

    app update

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
-import dash_bootstrap_components as dbc
 import plotly.express as px
 import plotly
 import plotly.io as pio
